9a1c883709971384478f4794c52e6b497f5b4044
unknown <asis8515@gmail.com>
2020-06-30 18:52:55 +0530

    Update Tempates

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,7 +1,9 @@
 <template>
     <Page class="page">
-        <ActionBar class="action-bar" title="Add Person"></ActionBar>
+        <ActionBar class="action-bar" :title="title()"></ActionBar>
         <StackLayout class="form" row="0" col="0">
+            <Button text="Add Person" v-if="!isAdd && !isEdit" @tap="add" class="btn btn-primary" />
+            <StackLayout v-if="isAdd || isEdit">
                 <StackLayout class="input-field">
                     <Label text="First Name" class="label font-weight-bold" />
                     <TextField class="input" v-model="input.firstname" />
@@ -14,20 +16,21 @@
                 </StackLayout>
                 <GridLayout rows="auto, auto" columns="*, *">
                     <Button text="Save" @tap="save" class="btn btn-primary" row="0" col="0" />
-                    <Button text="Clear" @tap="clear" class="btn btn-primary" row="0" col="1" colSpan="2"  />
+                    <Button text="Cancel" @tap="cancel" class="btn btn-primary" row="0" col="1" colSpan="2"  />
                 </GridLayout>
-
-                <ScrollView orientation="vertical" height="100%">
+            </StackLayout>
+            <ScrollView orientation="vertical" height="100%" v-if="isList">
                 <ListView for="person in $store.state.data" row="1" col="0">
                     <v-template>
-                        <StackLayout class="list-group-item">
-                            <Label v-bind:text="person.firstname + ' ' + person.lastname" />
-                        </StackLayout>
+                        <GridLayout columns="*, 40, 40" class="list-group-item">
+                            <Label col="0" v-bind:text="person.firstname + ' ' + person.lastname" />
+                            <Image col="1" src="~/assets/images/edit.png" style="background:green;padding:5;border-radius:5" @tap="edit(person)" width="25" height="25" class="right" vericalAlignment="center"/>
+                            <Image col="2" src="~/assets/images/delete.png" style="background:red;padding:5;border-radius:5" @tap="remove(person)" width="25" height="25" class="right" vericalAlignment="center"/>
+                        </GridLayout>
                     </v-template>
                 </ListView>
-                </ScrollView>
-
-            </StackLayout>
+            </ScrollView>
+        </StackLayout>
     </Page>
 </template>
 
@@ -37,8 +40,11 @@
         return {
             input: {
                 firstname: "",
-                lastname: ""
-            }
+                lastname: "",
+            },
+            isAdd:false,
+            isEdit:false,
+            isList:true,
         }
     },
     mounted() {
@@ -46,16 +52,52 @@
     },
     methods: {
         save() {
+            if(!this.input.firstname || !this.input.lastname) {
+                alert('First name and last name required.');
+                return;
+            }
             this.$store.dispatch("insert", this.input);
             this.clear();
             this.load();
         },
         load() {
+            this.isAdd = false;
+            this.isEdit = false;
+            this.isList = true;
             this.$store.dispatch("query");
         },
         clear() {
             this.input.firstname = "";
             this.input.lastname = "";
+        },
+        add() {
+            this.isAdd = true;
+            this.isEdit = true;
+            this.isList = false;
+            this.clear();
+        },
+        cancel() {
+            this.isAdd = false;
+            this.isEdit = false;
+            this.isList = true;
+        },
+        title() {
+            if(this.isAdd)
+                return 'Add Person';
+            else if(this.isEdit)
+                return 'Edit Person';
+            else
+                return 'Person';
+        },
+        edit(person) {
+            this.isAdd = false;
+            this.isEdit = true;
+            this.isList = false;
+            this.input.firstname = person.firstname;
+            this.input.lastname = person.lastname;
+        },
+        remove(person) {
+            alert(person.firstname+' '+person.lastname+' deleted');
         }
     }
   }
